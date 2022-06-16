9628af5d2b8c2a45a36517914132ce4ec52c8311
lofbgmlve <lofbgmlve@naver.com>
2022-06-16 10:43:37 +0900

    Hello.js Event test

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,18 +1,32 @@
 import World from "./World";
 import styles from "./Hello.module.css";
 export default function Hello(){
+
+  function showName(){
+    console.log("mike");
+  }
+  function showAge(age){
+    console.log(age);
+  }
+
+ // function showText(e){
+  //  console.log(e.target.value);
+  //}
   return ( 
   <div>
-       <h1 style={
-{
-  color:'#f00',
-  borderRight : '2px solid #000',
-  marginBottom : '30px',
-  opacity:1,
-}
-        
-      }>Hello</h1>
-   <div className={styles.box}>Hello</div>
+       <h1>Hello</h1>
+      <button onClick={showName}>show name</button>
+      <button onClick={() => {
+        showAge(10);
+      }}
+       >
+        show age
+        </button>
+       <input type="text" 
+       onChange={e => {
+        console.log(e.target.value);
+       }}
+       />
   </div>
   );
 }
