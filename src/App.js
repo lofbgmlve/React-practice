a3ee61c6669c5935520dacd4fd0742e540b65a14
lofbgmlve <lofbgmlve@naver.com>
2022-06-16 10:19:27 +0900

    react css Test

--- src/App.js
+++ src/App.js
@@ -2,21 +2,16 @@
 import './App.css';
 import Hello from './component/Hello';
 import Welcome from './component/Welcome';
+import styles from "./App.module.css";
 
 function App() {
  
   return (
     <div className="App">
-      <h1 style={
-{
-  color:'#f00',
-  borderRight : '2px solid #000'
-}
-        
-      }>Hello</h1>
+    
       <Hello />
-      <Hello />
-      <Welcome />
+     
+      <div className={styles.box}>App</div>
     </div>
   );
 }
